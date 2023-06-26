a1a1b6e2d6b178918d924729ff684bea342af4b8
DennisHSLeiden <dennisleyting@gmail.com>
2023-06-26 18:17:56 +0200

    nieuwe pagina waar je een video kan maken

--- lib/full_video_page.dart
+++ lib/full_video_page.dart
@@ -1,6 +1,37 @@
-final double width = MediaQuery.of(context).size.width;
-    final PageController pageController = PageController(viewportFraction: 0.8);
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+import 'mijn_video_page.dart';
+
+class VideoPage extends StatefulWidget {
+  final String filePath;
+
+  const VideoPage({Key? key, required this.filePath}) : super(key: key);
+
+  @override
+  _VideoPageState createState() => _VideoPageState();
+}
+
+class _VideoPageState extends State<VideoPage> {
+  late VideoPlayerController _videoPlayerController;
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  Future _initVideoPlayer() async {
+    _videoPlayerController = VideoPlayerController.file(File(widget.filePath));
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true);
+    await _videoPlayerController.play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dovenlingo'),
@@ -48,6 +79,7 @@ final double width = MediaQuery.of(context).size.width;
           ),
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
