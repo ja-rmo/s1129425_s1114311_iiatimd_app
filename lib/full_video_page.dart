a0bdbbdd37cba98b0ad17e7978acc2c5fe6dd584
DennisHSLeiden <dennisleyting@gmail.com>
2023-06-26 17:19:02 +0200

    naam verandering

--- lib/full_video_page.dart
+++ lib/full_video_page.dart
@@ -1,105 +1,53 @@
-import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+final double width = MediaQuery.of(context).size.width;
+    final PageController pageController = PageController(viewportFraction: 0.8);
 
-import 'mijn_video_page.dart';
-
-class VideoPage extends StatefulWidget {
-  const VideoPage({Key? key}) : super(key: key);
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _VideoPageState createState() => _VideoPageState();
-}
-
-class _VideoPageState extends State<VideoPage> {
-  late VideoPlayerController _controller;
-  bool _isVideoInitialized = false;
-  bool _isPlaying = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller =
-        VideoPlayerController.asset('assets/videos/Mijn_naam_is_Suzette.mp4')
-          ..initialize().then((_) {
-            setState(() {
-              _isVideoInitialized = true;
-            });
-          });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Mijn naam is {}'),
+        title: const Text('Dovenlingo'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Wij leren vandaag de zin: Mijn naam is {}!',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          Container(
+            height: 40,
+            width: 100,
+            alignment: Alignment.center,
+            child: SmoothPageIndicator(
+              controller: pageController,
+              count: 3,
             ),
           ),
-          Expanded(
-            child: Center(
-              child: _isVideoInitialized
-                  ? Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        AspectRatio(
-                          aspectRatio: _controller.value.aspectRatio,
-                          child: VideoPlayer(_controller),
-                        ),
-                        Positioned(
-                          bottom: 16,
-                          right: 16,
-                          child: FloatingActionButton(
-                            onPressed: () {
-                              setState(() {
-                                _isPlaying = !_isPlaying;
-                                if (_isPlaying) {
-                                  _controller.play();
-                                } else {
-                                  _controller.pause();
-                                }
-                              });
-                            },
-                            child: Icon(
-                              _isPlaying ? Icons.pause : Icons.play_arrow,
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  : const CircularProgressIndicator(),
+          SizedBox(
+            height: 250,
+            width: width,
+            child: PageView(
+              controller: pageController,
+              children: [
+                Chapter(
+                  headline: "Hoofdstuk 1",
+                  description: "Introductie",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/hoofdstuk1_navigatie');
+                  },
+                ),
+                Chapter(
+                  headline: "Hoofdstuk 2",
+                  description: "Introductie",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/hoofdstuk2_navigatie');
+                  },
+                ),
+                Chapter(
+                  headline: "Hoofdstuk 3",
+                  description: "Introductie",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/hoofdstuk3_navigatie');
+                  },
+                ),
+              ],
             ),
           ),
         ],
       ),
-      floatingActionButton: Container(
-        margin: const EdgeInsets.all(16),
-        width: double.infinity,
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const MijnVideoPage()),
-            );
-          },
-          child: const Text('Aan de slag'),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
