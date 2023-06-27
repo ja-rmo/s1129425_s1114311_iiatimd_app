4f01c83f672f130e3abe9dd37ab8bf6a06412272
ja-rmo <jarmo.t.haasnoot@gmail.com>
2023-06-27 14:06:05 +0200

    generated_files

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_media_info
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterMediaInfoPlugin.register(with: registry.registrar(forPlugin: "FlutterMediaInfoPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
