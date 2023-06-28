d0835e90a2269dc2cb803e393e8a5d4197c36b7f
ja-rmo <jarmo.t.haasnoot@gmail.com>
2023-06-28 15:32:06 +0200

    video-comparison first draft

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-// import 'dart:ui';
-
 import 'package:flutter/material.dart';
-// import 'package:flutter_dovenlingo/camera_page.dart';
 import 'package:flutter_dovenlingo/video_player_widget.dart';
-// import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-// import 'package:flutter_media_info/flutter_media_info.dart';
-// import 'chapter.dart';
+
+void main() {
+  runApp(const MainApp());
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({Key? key}) : super(key: key);
@@ -14,7 +12,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Dovenlingo',
       theme: ThemeData(primarySwatch: Colors.green),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
@@ -29,55 +27,25 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const VideoPlayerWidget();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Dovenlingo')),
+      body: ListView(
+        children: [
+          Container(
+              height: 300,
+              width: 200,
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
+              child: const VideoPlayerWidget(videoUrl: 'assets/videos/cat-jam.mp4')),
+          Container(
+              height: 300,
+              width: 200,
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
+              child: const VideoPlayerWidget(
+                  videoUrl: 'assets/videos/the-more-you-know.mp4')),
+        ],
+      ),
+    );
   }
-}
-
-// class HomePage extends StatefulWidget {
-//   const HomePage({Key? key}) : super(key: key);
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   final PageController _pageController = PageController(viewportFraction: 0.8);
-//   final double width = window.physicalSize.width;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Dovenlingo'),
-//         ),
-//         body: Column(children: [
-//           Container(
-//               height: 40,
-//               width: 100,
-//               alignment: Alignment.center,
-//               child:
-//                   SmoothPageIndicator(controller: _pageController, count: 3)),
-//           SizedBox(
-//             height: 250,
-//             width: width,
-//             child: PageView(controller: _pageController, children: [
-//               Chapter(
-//                   headline: "hoofdstuk 1",
-//                   description: "introductie",
-//                   onPressed: () => Navigator.pushNamed(context, '/hoofdstuk1')),
-//               Chapter(
-//                   headline: "hoofdstuk 2",
-//                   description: "introductie",
-//                   onPressed: () => Navigator.pushNamed(context, '/hoofdstuk2')),
-//               Chapter(
-//                   headline: "hoofdstuk 3",
-//                   description: "introductie",
-//                   onPressed: () => Navigator.pushNamed(context, '/hoofdstuk3'))
-//             ]),
-//           ),
-//         ]));
-//   }
-// }
-
-void main() {
-  runApp(const MainApp());
 }
