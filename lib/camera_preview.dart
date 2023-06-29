a37d9895157ed715555250999b1158502b71b7b2
ja-rmo <jarmo.t.haasnoot@gmail.com>
2023-06-29 14:26:23 +0200

    camera preview and record is fucking working yeee

---
+++ lib/camera_preview.dart
@@ -0,0 +1,367 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:video_player/video_player.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/scheduler.dart';
+
+List<CameraDescription> cameras = <CameraDescription>[];
+
+class CameraPreviewWidget extends StatefulWidget {
+  const CameraPreviewWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CameraPreviewWidget> createState() {
+    return _CameraPreviewWidgetState();
+  }
+}
+
+IconData getCameraLensIcon(CameraLensDirection direction) {
+  switch (direction) {
+    case CameraLensDirection.back:
+      return Icons.camera_rear;
+    case CameraLensDirection.front:
+      return Icons.camera_front;
+    case CameraLensDirection.external:
+      return Icons.camera;
+  }
+  // This enum is from a different package, so a new value could be added at
+  // any time. The example should keep working if that happens.
+  // ignore: dead_code
+  return Icons.camera;
+}
+
+void _logError(String code, String? message) {
+  // ignore: avoid_print
+  print('Error: $code${message == null ? '' : '\nError Message: $message'}');
+}
+
+class _CameraPreviewWidgetState extends State<CameraPreviewWidget>
+    with WidgetsBindingObserver, TickerProviderStateMixin {
+  CameraController? camController;
+  XFile? imageFile;
+  XFile? videoFile;
+  VideoPlayerController? videoController;
+  VoidCallback? videoPlayerListener;
+  bool enableAudio = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _initializeCamera();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController? cameraController = camController;
+
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      _initializeCameraController(cameraController.description);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black,
+              border: Border.all(
+                color: camController != null &&
+                        camController!.value.isRecordingVideo
+                    ? Colors.redAccent
+                    : Colors.grey,
+                width: 3.0,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: Center(
+                child: _cameraPreviewWidget(),
+              ),
+            ),
+          ),
+        ),
+        _captureControlRowWidget(),
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Row(
+            children: <Widget>[
+              _cameraTogglesRowWidget(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _cameraPreviewWidget() {
+    final CameraController? cameraController = camController;
+
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return Container();
+    }
+
+    return AspectRatio(
+      aspectRatio: cameraController.value.aspectRatio,
+      child: CameraPreview(cameraController),
+    );
+  }
+
+  Widget _captureControlRowWidget() {
+    final CameraController? cameraController = camController;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.videocam),
+          color: Colors.blue,
+          onPressed: cameraController != null &&
+                  cameraController.value.isInitialized &&
+                  !cameraController.value.isRecordingVideo
+              ? onVideoRecordButtonPressed
+              : null,
+        ),
+        IconButton(
+          icon: const Icon(Icons.stop),
+          color: Colors.red,
+          onPressed: cameraController != null &&
+                  cameraController.value.isInitialized &&
+                  cameraController.value.isRecordingVideo
+              ? onStopButtonPressed
+              : null,
+        ),
+        IconButton(
+          icon: const Icon(Icons.pause_presentation),
+          color:
+              cameraController != null && cameraController.value.isPreviewPaused
+                  ? Colors.red
+                  : Colors.blue,
+          onPressed:
+              cameraController == null ? null : onPausePreviewButtonPressed,
+        ),
+      ],
+    );
+  }
+
+  Widget _cameraTogglesRowWidget() {
+    final List<Widget> toggles = <Widget>[];
+
+    void onChanged(CameraDescription? description) {
+      if (description == null) {
+        return;
+      }
+
+      onNewCameraSelected(description);
+    }
+
+    if (cameras.isEmpty) {
+      SchedulerBinding.instance.addPostFrameCallback((_) async {
+        showInSnackBar('No camera found.');
+      });
+      return const Text('None');
+    } else {
+      for (final CameraDescription cameraDescription in cameras) {
+        toggles.add(
+          SizedBox(
+            width: 90.0,
+            child: RadioListTile<CameraDescription>(
+              title: Icon(getCameraLensIcon(cameraDescription.lensDirection)),
+              groupValue: camController?.description,
+              value: cameraDescription,
+              onChanged: onChanged,
+            ),
+          ),
+        );
+      }
+    }
+
+    return Row(children: toggles);
+  }
+
+  void showInSnackBar(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  Future<void> _getAvailableCameras() async {
+    try {
+      cameras = await availableCameras();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+  }
+
+  Future<void> _initializeCamera() async {
+  await _getAvailableCameras();
+  if (cameras.isNotEmpty) {
+    await _initializeCameraController(cameras[0]);
+  }
+}
+
+  Future<void> onNewCameraSelected(CameraDescription cameraDescription) async {
+    if (camController != null) {
+      return camController!.setDescription(cameraDescription);
+    } else {
+      return _initializeCameraController(cameraDescription);
+    }
+  }
+
+  Future<void> _initializeCameraController(
+      CameraDescription cameraDescription) async {
+    final CameraController cameraController = CameraController(
+      cameraDescription,
+      ResolutionPreset.medium,
+      enableAudio: enableAudio,
+    );
+
+    // If the camController is updated then update the UI.
+    cameraController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+      if (cameraController.value.hasError) {
+        showInSnackBar(
+            'Camera error ${cameraController.value.errorDescription}');
+      }
+    });
+
+    try {
+      await cameraController.initialize();
+      camController = cameraController;
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  void onVideoRecordButtonPressed() {
+    startVideoRecording().then((_) {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+  }
+
+  void onStopButtonPressed() {
+    stopVideoRecording().then((XFile? file) {
+      if (mounted) {
+        setState(() {});
+      }
+      if (file != null) {
+        showInSnackBar('Video recorded to ${file.path}');
+        videoFile = file;
+        _startVideoPlayer();
+      }
+    });
+  }
+
+  Future<void> onPausePreviewButtonPressed() async {
+    final CameraController? cameraController = camController;
+
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      showInSnackBar('Error: select a camera first.');
+      return;
+    }
+
+    if (cameraController.value.isPreviewPaused) {
+      await cameraController.resumePreview();
+    } else {
+      await cameraController.pausePreview();
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<void> startVideoRecording() async {
+    final CameraController? cameraController = camController;
+
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      showInSnackBar('Error: select a camera first.');
+      return;
+    }
+
+    if (cameraController.value.isRecordingVideo) {
+      return;
+    }
+
+    try {
+      await cameraController.startVideoRecording();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return;
+    }
+  }
+
+  Future<XFile?> stopVideoRecording() async {
+    final CameraController? cameraController = camController;
+
+    if (cameraController == null || !cameraController.value.isRecordingVideo) {
+      return null;
+    }
+
+    try {
+      return cameraController.stopVideoRecording();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+  }
+
+  Future<void> _startVideoPlayer() async {
+    if (videoFile == null) {
+      return;
+    }
+
+    final VideoPlayerController vController =
+        VideoPlayerController.file(File(videoFile!.path));
+
+    videoPlayerListener = () {
+      if (videoController != null) {
+        // Refreshing the state to update video player with the correct ratio.
+        if (mounted) {
+          setState(() {});
+        }
+        videoController!.removeListener(videoPlayerListener!);
+      }
+    };
+    vController.addListener(videoPlayerListener!);
+    await vController.setLooping(true);
+    await vController.initialize();
+    await videoController?.dispose();
+    if (mounted) {
+      setState(() {
+        imageFile = null;
+        videoController = vController;
+      });
+    }
+    await vController.play();
+  }
+
+  void _showCameraException(CameraException e) {
+    _logError(e.code, e.description);
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'video_player_widget.dart';
+import 'camera_preview.dart';
 
-void main() {
+Future<void> main() async{
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+  } catch (e) {
+    print(e);
+  }
   runApp(const MainApp());
 }
+
 
 class MainApp extends StatelessWidget {
   const MainApp({Key? key}) : super(key: key);
@@ -29,23 +36,34 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Dovenlingo')),
-      body: ListView(
-        children: [
-          Container(
-              height: 300,
-              width: 200,
-              margin: const EdgeInsets.all(20),
-              padding: const EdgeInsets.all(20),
-              child: const VideoPlayerWidget(videoUrl: 'assets/videos/cat-jam.mp4')),
-          Container(
-              height: 300,
-              width: 200,
-              margin: const EdgeInsets.all(20),
-              padding: const EdgeInsets.all(20),
-              child: const VideoPlayerWidget(
-                  videoUrl: 'assets/videos/the-more-you-know.mp4')),
-        ],
-      ),
+      body: const CameraPreviewWidget(),
     );
   }
 }
+
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Dovenlingo')),
+//       body: ListView(
+//         children: [
+//           Container(
+//               height: 300,
+//               width: 200,
+//               margin: const EdgeInsets.all(20),
+//               padding: const EdgeInsets.all(20),
+//               child: const VideoPlayerWidget(videoUrl: 'assets/videos/cat-jam.mp4')),
+//           Container(
+//               height: 300,
+//               width: 200,
+//               margin: const EdgeInsets.all(20),
+//               padding: const EdgeInsets.all(20),
+//               child: const VideoPlayerWidget(
+//                   videoUrl: 'assets/videos/the-more-you-know.mp4')),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
